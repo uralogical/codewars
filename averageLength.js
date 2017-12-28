19ad45a4567d2f575786b5e8150dbdccb88fff51
masatoshi.atsumi <masatoshi.atsumi@seattleconsulting.co.jp>
2017-12-28 10:31:02 +0900

    submit

--- averageLength.js
+++ averageLength.js
@@ -1,8 +1,25 @@
 function averageLength() { 
-	array_length = arguments[0].length;
-	
-	console.log(array_length);
+	var args = arguments[0];
+	var length_ave = getAverageLength(args);
+	var result = [];
+
+	args.forEach(function(arg, index) {
+		result.push( arg[0].repeat(length_ave));
+	})
+
+	return result;
 }
 
+function getAverageLength(args) {
+
+	var args_length = args.length;
+	var length_sum = 0;
+
+	args.forEach(function(elm, index) {
+		length_sum += elm.length;
+	})
+
+	return Math.round(length_sum / args_length);
+}
 
 averageLength(['aa', 'bb', 'ddd', 'eee']);
